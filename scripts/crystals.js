21797f11507afc4a38f74cf15ad4f159131a105b
Brett Fuller <bfuller123@gmail.com>
2017-05-13 23:20:33 -0500

    clicking crystals adds to score. target and current scores displayed on page

--- scripts/crystals.js
+++ scripts/crystals.js
@@ -9,9 +9,21 @@ var crystalFunctions = {
 };
 
 var crystalValues = {
-  crystalOne: crystalFunctions.getCrystalValue(),
-  crystalTwo: crystalFunctions.getCrystalValue(),
-  crystalThree: crystalFunctions.getCrystalValue(),
-  crystalFour: crystalFunctions.getCrystalValue(),
-  targetScore: crystalFunctions.getTargetScore()
+  'crystalOne': crystalFunctions.getCrystalValue(),
+  'crystalTwo': crystalFunctions.getCrystalValue(),
+  'crystalThree': crystalFunctions.getCrystalValue(),
+  'crystalFour': crystalFunctions.getCrystalValue(),
+  targetScore: crystalFunctions.getTargetScore(),
+  currentScore: 0
 }
+
+$(document).ready(function(){
+  $('.target_score').html(crystalValues.targetScore);
+
+  $('.crystal').on('click', function(){
+    var name = $(this).attr('name');
+    crystalValues.currentScore = crystalValues.currentScore + crystalValues[name];
+    $('.current_score').html(crystalValues.currentScore);
+  });
+
+});
