aaa4aab991079f7b6e38343868f07f706dd70ad4
Brett Fuller <bfuller123@gmail.com>
2017-05-14 21:00:24 -0500

    added gemBoss

--- scripts/crystals.js
+++ scripts/crystals.js
@@ -14,16 +14,30 @@ var crystalValues = {
   'crystalThree': crystalFunctions.getCrystalValue(),
   'crystalFour': crystalFunctions.getCrystalValue(),
   targetScore: crystalFunctions.getTargetScore(),
-  currentScore: 0
+  currentScore: 0,
+  takeHomePay: 0
 }
+
 
 $(document).ready(function(){
   $('.target_score').html(crystalValues.targetScore);
 
   $('.crystal').on('click', function(){
     var name = $(this).attr('name');
+
     crystalValues.currentScore = crystalValues.currentScore + crystalValues[name];
     $('.current_score').html(crystalValues.currentScore);
+
+    if (crystalValues.currentScore == crystalValues.targetScore) {
+      $('.alert').html('Great work for the day! I am giving you a bonus.');
+      crystalValues.currentScore = crystalValues.currentScore * 1.5;
+      crystalValues.takeHomePay = crystalValues.takeHomePay + crystalValues.currentScore;
+      $('.take_home_pay').html(crystalValues.takeHomePay);
+    }
+    else if (crystalValues.currentScore > crystalValues.targetScore) {
+      crystalValues['crystalOne'] = crystalValues['crystalTwo'] = crystalValues['crystalThree'] = crystalValues['crystalFour'] = 0;
+      $('.alert').html('Cave in! You lose');
+    }
   });
 
 });
---
+++ scripts/gemBoss.js
@@ -0,0 +1,55 @@
+
+var gemBoss = {
+  calculatePay: function(){
+    var payment = crystalFunctions.currentScore * (Math.floor(Math.random() * 3)+1);
+    return payment;
+  },
+  payEmployee: function(){
+    var payment = gemBoss.calculatePay();
+    $('.alert').append('<p>Your take home pay for the day is ' + payment + '.</p>');
+    crystalValues.takeHomePay = crystalValues.takeHomePay + payment;
+    $('.take_home_pay').html(crystalValues.takeHomePay);
+  },
+  dockingPay: function(){
+    crystalValues.currentScore = crystalValues.currentScore - crystalFunctions.getCrystalValue();
+    $('.current_score').html(crystalValues.currentScore);
+  },
+  dockPay: function(){
+    $('.alert').html("I guess this is enough...but I'm docking your pay!");
+    gemBoss.dockingPay();
+    gemBoss.payEmployee();
+  },
+  satisfied: function(){
+    $('.alert').html("While I'm not thrilled...this works for me.");
+    gemBoss.payEmployee();
+  },
+  fireYou: function(){
+    crystalValues['crystalOne'] = crystalValues['crystalTwo'] = crystalValues['crystalThree'] = crystalValues['crystalFour'] = 0;
+    $('.alert').html("You didn't bring me enough crystals. You're Fired!");
+    $('.alert').append('<p>Game over!</p>');
+  },
+  backToWork: function(){
+    $('.alert').html("You can't leave the mine yet! Get back to work!");
+  },
+  randomArrayChoice: function(arr){
+    var randomChoice = Math.floor(Math.random()*arr.length);
+    return arr[randomChoice];
+  },
+  getBossReaction: function(){
+    var reaction = gemBoss.randomArrayChoice(reactions);
+    if (reaction == 'dockPay'){
+      gemBoss.dockPay();
+    }
+    else if (reaction == 'satisfied') {
+      gemBoss.satisfied();
+    }
+    else if (reaction == 'fireYou') {
+      gemBoss.fireYou();
+    }
+    else {
+      gemBoss.backToWork();
+    }
+  }
+};
+
+var reactions = ['dockPay', 'dockPay', 'satisfied', 'satisfied', 'fireYou', 'backToWork'];
